495d2f3dd6634f1b0a2857b44995d58b222a5d53
theforestvn88 <theforestvn88@gmail.com>
2024-02-11 15:55:59 +0700

    init gem

---
+++ lib/rails_dbs_load_balancer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_dbs_load_balancer/version"
+
+module RailsDbsLoadBalancer
+  class Error < StandardError; end
+  # Your code goes here...
+end
